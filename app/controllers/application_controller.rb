938eead926744e081061075919ee9e5c89cef482
inamu108 <inamublog@gmail.com>
2020-07-26 13:59:31 +0900

    user-edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :email, :family_name, :first_name])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
+  
   def show
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    # params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :family_name, :first_name)
+    params.require(:user).permit(:nickname, :email, :family_name, :first_name)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
