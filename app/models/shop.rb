352fdf92924db99a050770b8551a01f1d5da5d01
inamu108 <inamublog@gmail.com>
2020-07-29 11:10:56 +0900

    generate model like

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,6 +3,12 @@ class Shop < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes
+  has_many :users, through: :likes
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 
   mount_uploader :image, ImageUploader
 
